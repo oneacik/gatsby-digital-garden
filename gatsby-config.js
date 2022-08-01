245b0b4bd6433dff229569c0e21d5ee9beab095f
psuwala <oneat@vp.pl>
2022-08-01 23:36:36 +0200

    removed purgecss

--- gatsby-config.js
+++ gatsby-config.js
@@ -123,21 +123,6 @@ module.exports = {
     // },
 
     {
-      resolve: `gatsby-plugin-purgecss`,
-      options: {
-        // printRejected: true, // Print removed selectors and processed file names. Use for debugging.
-        // develop: true, // Enable while using `gatsby develop`
-        // tailwind: true, // Enable tailwindcss support
-        ignore: ['tippy.js/', 'tooltip.css'], // Ignore files/folders
-        // purgeOnly : ['components/', '/main.css', 'bootstrap/'], // Purge only these files/folders
-        purgeCSSOptions: {
-          safelist: ['.tippy-box'], // Don't remove this selector
-        },
-        // More options defined here https://purgecss.com/configuration.html#options
-      },
-    },
-
-    {
       resolve: 'gatsby-plugin-local-search',
       options: {
         // A unique name for the search index. This should be descriptive of
